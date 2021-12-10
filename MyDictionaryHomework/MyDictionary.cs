56e4e8008024dae0f6bcce954d90251a5a602848
Tinamur <bozdoganm.6@gmail.com>
2021-12-10 20:03:24 +0300

    Add project files.

---
+++ MyDictionaryHomework/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryHomework
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] tKeys;
+        TValue[] tValues;
+
+        public MyDictionary()
+        {
+            tKeys = new TKey[0];
+            tValues = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (Array.IndexOf(tKeys, key) == -1)
+            {
+                TKey[] tempKeys = tKeys;
+                TValue[] tempValues = tValues;
+                tKeys = new TKey[tKeys.Length + 1];
+                tValues = new TValue[tValues.Length + 1];
+                for (int i = 0; i < tempKeys.Length; i++)
+                {
+                    tKeys[i] = tempKeys[i];
+                    tValues[i] = tempValues[i];
+                }
+                tKeys[tKeys.Length-1] = key;
+                tValues[tValues.Length-1] = value;
+            }
+            else
+            {
+                Console.WriteLine("Anahtar Zaten Kullanılıyor");
+            }
+
+        }
+
+        public void GetDictionary()
+        {
+            for (int i = 0; i < tKeys.Length; i++)
+            {
+                Console.WriteLine("key : " + tKeys[i]+ " Value : " + tValues[i]);
+            }
+
+        }
+
+
+    }
+}
---
+++ MyDictionaryHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isimler = new MyDictionary<int, string>();
+            isimler.Add(20, "İsim_1");
+            isimler.Add(20, "İsim_2"); // Kullanılan Key Hatası Geliyor mu Test
+            isimler.Add(21, "İsim_3"); 
+            isimler.Add(22, "İsim_3"); // Aynı value sorun çıkartmamalı!
+            isimler.Add(23, "İsim_5");
+
+            isimler.GetDictionary();  // Yazdırsın kendini görelim
+        }
+    }
+}
